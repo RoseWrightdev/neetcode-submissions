4d44698c27a0705a1c8300b97bf8890841cf696f
Rose Wright <rosesophiewright@gmail.com>
2026-07-06 22:06:40 -0500

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = range(1, n+1)
+        path = []
+        combs = []
+        def dfs(i):
+            if i >= n:
+                if len(path) == k:
+                    combs.append(path[:])
+                return
+            path.append(nums[i])
+            dfs(i + 1)
+            path.pop()
+            dfs(i + 1)
+        dfs(0)
+        return combs
